357c9d69dced3aa5aed4ba73b965e62a540f9cde
Brian Rivera <69652100+BraitoRX@users.noreply.github.com>
2024-05-24 18:22:09 -0500

    Configuración inicial

--- Validación de datos/puppeteer-cucumber-validacion-datos/step_definitions/support/constants.js
+++
@@ -1,9 +0,0 @@
-let config = require('konfig')({path: './config'})
-
-module.exports = {
-  baseUrl: config.properties.baseUrl,
-  pageTimeout: config.properties.pageTimeout,
-  headlessMode: config.properties.headlessMode,
-  chromiumPath: config.properties.chromiumPath,
-  reportConfig: config.properties.reportConfig
-}
